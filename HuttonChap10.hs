7a4b33a610f45567a1d05cc0f513fbc7695897fd
Benjamin Purcell <>
2024-08-25 10:24:01 -0600

    Hutton Chapter 10, finished exercises

--- HuttonChap10.hs
+++ HuttonChap10.hs
@@ -1,5 +1,7 @@
+import Control.Concurrent (threadDelay)
 import Control.Monad (zipWithM_)
 import Data.Char (digitToInt, isDigit)
+import Data.List (nub)
 import System.IO (hSetEcho, stdin)
 
 act :: IO (Char, Char)
@@ -19,10 +21,13 @@ getLine' = do
             return $ c : s
 
 putStr' :: String -> IO ()
+{-
 putStr' [] = return ()
 putStr' (c : cs) = do
     putChar c
     putStr' cs
+-}
+putStr' = mapM_ putChar
 
 putStrLn' :: String -> IO ()
 putStrLn' s = do
@@ -82,18 +87,18 @@ next :: Int -> Int
 next 1 = 2
 next 2 = 1
 
-type Board = [Int]
+type NimBoard = [Int]
 
-initial :: Board
+initial :: NimBoard
 initial = [5, 4, 3, 2, 1]
 
-finished :: Board -> Bool
+finished :: NimBoard -> Bool
 finished = all (== 0)
 
-valid :: Board -> Int -> Int -> Bool
+valid :: NimBoard -> Int -> Int -> Bool
 valid board row stars = stars <= board !! (row - 1)
 
-move :: Board -> Int -> Int -> Board
+move :: NimBoard -> Int -> Int -> NimBoard
 move board row stars = zipWith update [1 ..] board
   where
     update r n = if r == row then n - stars else n
@@ -104,7 +109,7 @@ putRow row stars = do
     putStr ": "
     putStrLn $ unwords $ replicate stars "*"
 
-putBoard :: Board -> IO ()
+putBoard :: NimBoard -> IO ()
 putBoard = zipWithM_ putRow [1 ..]
 
 newline :: IO ()
@@ -121,7 +126,7 @@ getDigit prompt = do
             putStrLn "ERROR: Invalid digit"
             getDigit prompt
 
-playNim :: Board -> Int -> IO ()
+playNim :: NimBoard -> Int -> IO ()
 playNim board player = do
     newline
     putBoard board
@@ -145,4 +150,124 @@ playNim board player = do
                     playNim board player
 
 nim :: IO ()
-nim = play initial 1
+nim = playNim initial 1
+
+cls :: IO ()
+cls = putStr "\ESC[2J"
+
+type Pos = (Int, Int)
+
+writeat :: Pos -> String -> IO ()
+writeat p xs = do
+    goto p
+    putStr xs
+
+goto :: Pos -> IO ()
+goto (x, y) = putStr $ "\ESC[" ++ show y ++ ";" ++ show x ++ "H"
+
+width :: Int
+width = 10
+
+height :: Int
+height = 10
+
+type Board = [Pos]
+
+glider :: Board
+glider = [(4, 2), (2, 3), (4, 3), (3, 4), (4, 4)]
+
+showcells :: Board -> IO ()
+showcells = mapM_ (`writeat` "O")
+
+isAlive :: Board -> Pos -> Bool
+isAlive b p = p `elem` b
+
+isEmpty :: Board -> Pos -> Bool
+isEmpty b p = not $ isAlive b p
+
+neighbs :: Pos -> [Pos]
+neighbs (x, y) =
+    map
+        wrap
+        [ (x - 1, y - 1)
+        , (x, y - 1)
+        , (x + 1, y - 1)
+        , (x - 1, y)
+        , (x + 1, y)
+        , (x - 1, y + 1)
+        , (x, y + 1)
+        , (x + 1, y + 1)
+        ]
+
+wrap :: Pos -> Pos
+wrap (x, y) =
+    ( (x - 1) `mod` width + 1
+    , (y - 1) `mod` height + 1
+    )
+
+liveneighbs :: Board -> Pos -> Int
+liveneighbs b = length . filter (isAlive b) . neighbs
+
+survivors :: Board -> [Pos]
+survivors b = [p | p <- b, liveneighbs b p `elem` [2, 3]]
+
+births :: Board -> [Pos]
+births b =
+    [ p
+    | p <- nub $ concatMap neighbs b
+    , isEmpty b p
+    , liveneighbs b p == 3
+    ]
+
+nextgen :: Board -> Board
+nextgen b = survivors b ++ births b
+
+life :: Board -> IO ()
+life b = do
+    cls
+    showcells b
+    threadDelay 100000
+    life $ nextgen b
+
+-- Exercise 1-3 done above
+--
+-- Exercise 4.
+adder :: IO ()
+adder = do
+    n <- getDigit "How many numbers? "
+    s <- sum <$> mapM (\_ -> getDigit "") [1 .. n]
+    putStr "The total is "
+    print s
+
+-- Exercise 5.
+readLine :: IO String
+readLine = go ""
+  where
+    go s = do
+        c <- getCh
+        case c of
+            '\n' -> do
+                putChar '\n'
+                return $ reverse s
+            '\DEL' -> case s of
+                "" -> go []
+                (x : xs) -> do
+                    putChar '\b'
+                    go xs
+            _ -> do
+                putChar c
+                go (c : s)
+
+{-
+readLine = do
+    c <- getCh
+    case c of
+        '\n' -> return []
+        '\DEL' -> do
+            putChar '\b'
+            readLine
+        _ -> do
+            putChar c
+            s <- readLine
+            return (c : s)
+-}
