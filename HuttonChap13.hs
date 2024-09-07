342fed8108c869efa09ebcde732d74e9b5a34a02
Benjamin Purcell <>
2024-09-06 18:53:31 -0600

    Chapter 13, progress

--- HuttonChap13.hs
+++ HuttonChap13.hs
@@ -120,3 +120,30 @@ nats =
         <*> natural
         <*> many ((\_ n -> n) <$> symbol "," <*> natural)
         <*> symbol "]"
+
+expr :: Parser Int
+expr = do
+    t <- term
+    do
+        symbol "+"
+        e <- expr
+        return (t + e)
+        <|> return t
+
+term :: Parser Int
+term = do
+    f <- factor
+    do
+        symbol "*"
+        t <- term
+        return $ f * t
+        <|> return f
+
+factor :: Parser Int
+factor =
+    do
+        symbol "("
+        e <- expr
+        symbol ")"
+        return e
+        <|> natural
