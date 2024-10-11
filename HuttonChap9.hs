3b42282f67e73268e693dd2c995a6033e0a632e6
Benjamin Purcell <11407693+spewspews@users.noreply.github.com>
2024-10-10 20:04:56 -0600

    Hutton Chapter 9, finished

--- HuttonChap9.hs
+++ HuttonChap9.hs
@@ -1,4 +1,11 @@
-import Control.Monad (join)
+import Control.Monad (join, (>=>))
+import Data.List (delete)
+import Data.Maybe (mapMaybe)
+
+main :: IO ()
+main = do
+    print $ length possible
+    print $ length successful
 
 data Op = Add | Sub | Mul | Div
 
@@ -7,20 +14,6 @@ instance Show Op where
     show Sub = "-"
     show Mul = "*"
     show Div = "/"
-
-valid :: Op -> Int -> Int -> Bool
-valid Sub x y = x > y
-valid Div x y = x `mod` y == 0
-valid _ _ _ = True
-
-apply :: Op -> Int -> Int -> Maybe Int
-apply op x y = if valid op x y then return (o x y) else Nothing
-  where
-    o = case op of
-        Add -> (+)
-        Sub -> (-)
-        Mul -> (*)
-        Div -> div
 
 data Expr = Val Int | App Op Expr Expr
 
@@ -31,15 +24,6 @@ instance Show Expr where
         brak (Val n) = show n
         brak e = "(" ++ show e ++ ")"
 
-values :: Expr -> [Int]
-values (Val i) = [i]
-values (App _ x y) = values x ++ values y
-
-eval :: Expr -> Maybe Int
-eval (Val i) = if i > 0 then return i else Nothing
-eval (App o x y) = join $ apply o <$> eval x <*> eval y
-
--- Combinatorial Functions
 subs :: [a] -> [[a]]
 subs [] = [[]]
 subs (x : xs) = xss ++ map (x :) xss
@@ -53,32 +37,21 @@ interleave x l@(y : ys) = (x : l) : map (y :) (interleave x ys)
 perms :: [a] -> [[a]]
 perms = foldr (concatMap . interleave) [[]]
 
-{-
-perms = foldr f [[]]
-  where
-    f x xss = do
-        xs <- xss
-        interleave x xs
--}
-
+-- Exercise 1.
+-- choices l = [p | s <- subs l, p <- perms s]
 choices :: [a] -> [[a]]
-choices = concatMap perms . subs
+choices = subs >=> perms
 
-{-
-choices l = do
-    x <- subs l
-    perms x
--}
+-- Exercise 2.
+isChoice :: (Eq a) => [a] -> [a] -> Bool
+isChoice [] _ = True
+isChoice (x : xs) ys
+    | x `elem` ys = isChoice xs (delete x ys)
+    | otherwise = False
 
-solution :: Expr -> [Int] -> Int -> Bool
-solution e ns n =
-    elem (values e) (choices ns) && eval e == Just n
-
-bookExample :: Expr
-bookExample = App Mul (App Add (Val 1) (Val 50)) (App Sub (Val 25) (Val 10))
-
-isSolution :: Bool
-isSolution = solution bookExample [1, 3, 7, 10, 25, 50] 765
+-- Exercise 3.
+-- No effect, we would be drawing from an empty list
+-- meaning that we would not call results on that (empty) side.
 
 split :: [a] -> [([a], [a])]
 split [] = []
@@ -94,18 +67,26 @@ exprs ns = do
     r <- exprs rs
     combine l r
 
-{-
-exprs ns =
-    [ e
-    | (ls, rs) <- split ns
-    , l <- exprs ls
-    , r <- exprs rs
-    , e <- combine l r
-    ]
--}
-
 combine :: Expr -> Expr -> [Expr]
 combine l r = map (\o -> App o l r) [Add, Sub, Mul, Div]
 
-solutions :: [Int] -> Int -> [Expr]
-solutions l n = concatMap (filter ((== Just n) . eval) . exprs) $ choices l
+eval :: Expr -> Maybe Int
+eval (Val i) = Just i
+eval (App o x y) = join $ apply o <$> eval x <*> eval y
+
+valid :: Op -> Int -> Int -> Bool
+valid Div x y = y /= 0 && x `mod` y == 0
+valid _ _ _ = True
+
+apply :: Op -> Int -> Int -> Maybe Int
+apply op x y = if valid op x y then return (o x y) else Nothing
+  where
+    o = case op of
+        Add -> (+)
+        Sub -> (-)
+        Mul -> (*)
+        Div -> div
+
+-- Exercise 4.
+possible = concatMap exprs $ choices [1, 3, 7, 10, 25, 50]
+successful = mapMaybe eval possible
