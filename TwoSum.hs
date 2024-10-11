347292c9f5147f92267bf2e179ef41a2fde84d8c
Benjamin Purcell <11407693+spewspews@users.noreply.github.com>
2024-10-10 20:04:56 -0600

    TwoSum, some adjustments

--- TwoSum.hs
+++ TwoSum.hs
@@ -1,4 +1,5 @@
 import qualified Data.IntMap.Strict as IntMap
+import Data.Maybe
 
 twoSum :: Int -> [Int] -> [(Int, Int)]
 twoSum k = go mempty . zip [0 ..]
@@ -9,15 +10,15 @@ twoSum k = go mempty . zip [0 ..]
         ++ go (IntMap.alter (Just [i] <>) x seen) ixs
 
 twoSumMaybe :: Int -> [Int] -> [(Int, Int)]
-twoSumMaybe k = fromJust . go mempty . zip [0 ..]
+twoSumMaybe k = fromMaybe [] . go mempty . zip [0 ..]
   where
-    go seen [] = Just []
+    go seen [] = Nothing
     go seen ((i, x) : ixs) =
       (map (,i) <$> IntMap.lookup (k - x) seen)
         <> go (IntMap.alter (Just [i] <>) x seen) ixs
 
-twoSum' :: Int -> [Int] -> [(Int, Int)]
-twoSum' k xs = foldr go (const []) (zip [0 ..] xs) mempty
+twoSumFold :: Int -> [Int] -> [(Int, Int)]
+twoSumFold k xs = foldr go (const []) (zip [0 ..] xs) mempty
   where
     go (i, n) cont seen =
       map (,i) (IntMap.findWithDefault [] (k - n) seen)
