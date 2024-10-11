2d24f546262fb447cd29e3ab05d4260ae8bd4536
Benjamin Purcell <11407693+spewspews@users.noreply.github.com>
2024-10-10 20:04:56 -0600

    MaxCombAnd, add a mutable implementation

--- HuttonChap10.hs
+++ HuttonChap10.hs
@@ -1,3 +1,5 @@
+import Control.Monad (zipWithM_)
+import Data.Char (digitToInt, isDigit)
 import System.IO (hSetEcho, stdin)
 
 act :: IO (Char, Char)
@@ -35,6 +37,7 @@ strlen = do
     putStr $ show $ length s
     putStrLn " characters"
 
+-- Hangman
 hangman :: IO ()
 hangman = do
     putStrLn "Think of a word:"
@@ -73,3 +76,73 @@ play word = do
 
 match :: String -> String -> String
 match word guess = map (\c -> if c `elem` guess then c else '-') word
+
+-- Nim
+next :: Int -> Int
+next 1 = 2
+next 2 = 1
+
+type Board = [Int]
+
+initial :: Board
+initial = [5, 4, 3, 2, 1]
+
+finished :: Board -> Bool
+finished = all (== 0)
+
+valid :: Board -> Int -> Int -> Bool
+valid board row stars = stars <= board !! (row - 1)
+
+move :: Board -> Int -> Int -> Board
+move board row stars = zipWith update [1 ..] board
+  where
+    update r n = if r == row then n - stars else n
+
+putRow :: Int -> Int -> IO ()
+putRow row stars = do
+    putStr $ show row
+    putStr ": "
+    putStrLn $ unwords $ replicate stars "*"
+
+putBoard :: Board -> IO ()
+putBoard = zipWithM_ putRow [1 ..]
+
+newline :: IO ()
+newline = putChar '\n'
+
+getDigit :: String -> IO Int
+getDigit prompt = do
+    putStr prompt
+    x <- getChar
+    newline
+    if isDigit x
+        then return $ digitToInt x
+        else do
+            putStrLn "ERROR: Invalid digit"
+            getDigit prompt
+
+playNim :: Board -> Int -> IO ()
+playNim board player = do
+    newline
+    putBoard board
+    if finished board
+        then do
+            newline
+            putStr "Player "
+            putStr $ show $ next player
+            putStrLn " wins!!"
+        else do
+            newline
+            putStr "Player "
+            print player
+            row <- getDigit "Enter a row number: "
+            stars <- getDigit "Stars to remove: "
+            if valid board row stars
+                then playNim (move board row stars) $ next player
+                else do
+                    newline
+                    putStrLn "ERROR: Invalid move"
+                    playNim board player
+
+nim :: IO ()
+nim = play initial 1
