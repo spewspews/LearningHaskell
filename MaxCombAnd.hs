095275ef278424ff8f068bfde8d28bd58226e873
Benjamin Purcell <11407693+spewspews@users.noreply.github.com>
2024-10-10 20:04:56 -0600

    refactor to pointfree style

--- MaxCombAnd.hs
+++ MaxCombAnd.hs
@@ -13,19 +13,19 @@ pow (x : xs) = map (x :) powxs ++ powxs
     powxs = pow xs
 
 maxCombAndSlow :: [Int] -> Int
-maxCombAndSlow a =
-  fst $
-    head $
-      filter (\(_, v) -> v /= 0) $
-        map (\xs -> (length xs, foldl' (.&.) maxBound xs)) $
-          sortBy (\xs ys -> compare (length ys) (length xs)) $
-            pow a
+maxCombAndSlow =
+  fst
+    . head
+    . filter (\(_, v) -> v /= 0)
+    . map (\xs -> (length xs, foldl' (.&.) maxBound xs))
+    . sortBy (\xs ys -> compare (length ys) (length xs))
+    . pow
 
 bitList :: (Integral a) => a -> [a]
 bitList 0 = []
 bitList i = i `mod` 2 : bitList (i `div` 2)
 
 maxCombAnd :: [Int] -> Int
-maxCombAnd xs = V.maximum $ foldl' addBits (V.replicate 32 0) xs
+maxCombAnd = V.maximum . foldl' addBits (V.replicate 32 0)
   where
-    addBits v x = V.unsafeAccum (+) v $ zip [0 .. 31] $ bitList x
+    addBits v = V.unsafeAccum (+) v . zip [0 .. 31] . bitList
