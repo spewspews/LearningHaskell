faf7eaf5d2d89d5c4d2183ef5e3e46953d82ed25
Benjamin Purcell <11407693+spewspews@users.noreply.github.com>
2024-10-10 20:04:56 -0600

    Hutton Chapter 10, finished hangman

--- HuttonChap10.hs
+++ HuttonChap10.hs
@@ -1,3 +1,5 @@
+import System.IO (hSetEcho, stdin)
+
 act :: IO (Char, Char)
 act = do
     x <- getChar
@@ -32,3 +34,42 @@ strlen = do
     putStr "It has "
     putStr $ show $ length s
     putStrLn " characters"
+
+hangman :: IO ()
+hangman = do
+    putStrLn "Think of a word:"
+    word <- sgetLine
+    putStrLn "Try to guess it:"
+    play word
+
+sgetLine :: IO String
+sgetLine = do
+    x <- getCh
+    if x == '\n'
+        then do
+            putChar x
+            return []
+        else do
+            putChar '-'
+            xs <- sgetLine
+            return (x : xs)
+
+getCh :: IO Char
+getCh = do
+    hSetEcho stdin False
+    x <- getChar
+    hSetEcho stdin True
+    return x
+
+play :: String -> IO ()
+play word = do
+    putStr "? "
+    guess <- getLine
+    if guess == word
+        then putStrLn "You got it!!"
+        else do
+            putStrLn (match word guess)
+            play word
+
+match :: String -> String -> String
+match word guess = map (\c -> if c `elem` guess then c else '-') word
