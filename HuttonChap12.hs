16dc51d9ddf3c65f8aad58c01c07586589f49934
Benjamin Purcell <11407693+spewspews@users.noreply.github.com>
2024-10-10 20:04:56 -0600

    Hutton Chapter 12, progress

--- HuttonChap12.hs
+++ HuttonChap12.hs
@@ -1,3 +1,5 @@
+import Control.Monad (replicateM)
+
 data Tree a = Leaf a | Node (Tree a) (Tree a) deriving (Show, Functor)
 
 {-
@@ -6,3 +8,44 @@ instance Functor Tree where
     fmap g (Leaf x) = Leaf (g x)
     fmap g (Node l r) = Node (fmap g l) (fmap g r)
 -}
+
+getChars :: Int -> IO String
+getChars 0 = return []
+getChars n = (:) <$> getChar <*> getChars (n - 1)
+
+getChars' :: Int -> IO String
+getChars' n = replicateM n getChar
+
+pairs :: [a] -> [b] -> [(a, b)]
+pairs xs ys = do
+    x <- xs
+    y <- ys
+    return (x, y)
+
+pairs' :: [a] -> [b] -> [(a, b)]
+pairs' xs ys = (,) <$> xs <*> ys
+
+type State = Int
+
+newtype ST a = S {app :: State -> (a, State)}
+
+instance Functor ST where
+    -- fmap :: (a -> b) -> ST a -> ST b
+    fmap g st = S (\s -> let (x, s') = app st s in (g x, s'))
+
+instance Applicative ST where
+    -- pure :: a -> ST a
+    pure x = S (x,)
+
+    -- (<*>) :: ST (a -> b) -> ST a -> ST b
+    stf <*> stx =
+        S
+            ( \s ->
+                let (f, s') = app stf s
+                    (x, s'') = app stx s'
+                 in (f x, s'')
+            )
+
+instance Monad ST where
+    -- (>>=) :: ST a -> (a -> ST b) -> ST b
+    stx >>= f = S (\s -> let (x, s') = app stx s in app (f x) s')
