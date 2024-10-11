148de7d56eaeee1238d3a1f95f7db9224b2a2bf9
Benjamin Purcell <11407693+spewspews@users.noreply.github.com>
2024-10-10 20:04:56 -0600

    Hutton Chapter 9, progress

--- HuttonChap9.hs
+++ HuttonChap9.hs
@@ -3,10 +3,10 @@ import Control.Monad (join)
 data Op = Add | Sub | Mul | Div
 
 instance Show Op where
-  show Add = "+"
-  show Sub = "-"
-  show Mul = "*"
-  show Div = "/"
+    show Add = "+"
+    show Sub = "-"
+    show Mul = "*"
+    show Div = "/"
 
 valid :: Op -> Int -> Int -> Bool
 valid Sub x y = x > y
@@ -17,19 +17,19 @@ apply :: Op -> Int -> Int -> Maybe Int
 apply op x y = if valid op x y then return (o x y) else Nothing
   where
     o = case op of
-      Add -> (+)
-      Sub -> (-)
-      Mul -> (*)
-      Div -> div
+        Add -> (+)
+        Sub -> (-)
+        Mul -> (*)
+        Div -> div
 
 data Expr = Val Int | App Op Expr Expr
 
 instance Show Expr where
-  show (Val n) = show n
-  show (App o l r) = brak l ++ show o ++ brak r
-    where
-      brak (Val n) = show n
-      brak e = "(" ++ show e ++ ")"
+    show (Val n) = show n
+    show (App o l r) = brak l ++ show o ++ brak r
+      where
+        brak (Val n) = show n
+        brak e = "(" ++ show e ++ ")"
 
 values :: Expr -> [Int]
 values (Val i) = [i]
@@ -38,3 +38,20 @@ values (App _ x y) = values x ++ values y
 eval :: Expr -> Maybe Int
 eval (Val i) = if i > 0 then return i else Nothing
 eval (App o x y) = join $ apply o <$> eval x <*> eval y
+
+-- Combinatorial Functions
+subs :: [a] -> [[a]]
+subs [] = [[]]
+subs (x : xs) = xss ++ map (x :) xss
+  where
+    xss = subs xs
+
+interleave :: a -> [a] -> [[a]]
+interleave x [] = [[x]]
+interleave x l@(y : ys) = (x : l) : map (y :) (interleave x ys)
+
+perms :: [a] -> [[a]]
+perms = foldr (concatMap . interleave) [[]]
+
+choices :: [a] -> [[a]]
+choices = concatMap perms . subs
